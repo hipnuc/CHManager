119d7dfa8da3e39fde02fe27b096f78414a03e4d
Alex Yang <alex.yang@nxp.com>
2020-05-15 18:22:53 +0800

    CHManager: protocol: update to support new items
    Update prococol to support 0x91 and 0x62 items
    
    Signed-off-by: yandld<1453363089@qq.com>

--- CHManager/protocol/chhi22x.h
+++ CHManager/protocol/chhi22x.h
@@ -13,54 +13,47 @@
 
 typedef enum
 {
-    kItemTest8F =               0x60,
-    kItemExtend =               0x61,   /* extend data */
-    kItemRFQuat =               0x71,
-    kItemRFEul  =               0x72,
-    kItemRFAcc  =               0x75,
-    kItemRFGyr  =               0x78,
-    kItemRFMag  =               0x7A,
-    kItemRFData =               0x62,
+    kItemTest8F = 0x60,
+    kItemRFSOL =               0x62,   /* new RF data */
+    kItemIMUSOL =               0x91,   /* new IMU data */
     kItemID =                   0x90,   /* user programed ID    size: 1 */
     kItemAccRaw =               0xA0,   /* raw acc              size: 3x2 */
-    kItemAccCalibrated =        0xA1,
-    kItemAccFiltered =          0xA2,
-    kItemAccLinear   =          0xA5,
-    kItemAccGravity  =          0xA6,
-    kItemAccNorm =              0xA8,
     kItemGyrRaw =               0xB0,   /* raw gyro             size: 3x2 */
-    kItemGyrCalibrated =        0xB1,
-    kItemGyrFiltered =          0xB2,
-    kItemGyrNorm =              0xB8,
     kItemMagRaw =               0xC0,   /* raw mag              size: 3x2 */
-    kItemMagCalibrated =        0xC1,
-    kItemMagFiltered =          0xC2,
     kItemRotationEular =        0xD0,   /* eular angle          size:3x2 */
     kItemRotationEular2 =       0xD9,   /* new eular angle      size:3x4 */
     kItemRotationQuat =         0xD1,   /* att q,               size:4x4 */
-    kItemTemperature =          0xE0,
     kItemPressure =             0xF0,   /* pressure             size:1x4 */
     kItemEnd =                  0x00,
 }ItemID_t;
 
+
 typedef struct
 {
-    uint8_t id;
-    float       q[4];
-    int16_t     acc[3];
-    int16_t     gyr[3];
-    int16_t     mag[3];
-    float       eul[3];
-    float       temperature;
-    float       rf_quat[MAX_SLV_CNT][4];
-    int16_t     rf_acc[MAX_SLV_CNT][3];
-    int16_t     rf_gyr[MAX_SLV_CNT][3];
-    int16_t     rf_mag[MAX_SLV_CNT][3];
-    int16_t     rf_eul[MAX_SLV_CNT][3];
-}imu_data_t;
+    uint8_t     tag;                /* data packet tag */
+    uint8_t     id;
+    uint8_t     rev[6];             /* reserved */
+    uint32_t    ts;                 /* timestamp */
+    float       acc[3];
+    float       gyr[3];
+    float       mag[3];
+    float       eul[3];             /* eular angles:R/P/Y */
+    float       quat[4];            /* quaternion */
+}__attribute__((packed)) id0x91_t;
 
-void imu_data_parse(imu_data_t *imu, uint8_t *buf, uint32_t len);
+
+typedef struct
+{
+    uint8_t         tag;
+    uint8_t         gwid;
+    uint8_t         node_cnt;
+    uint8_t         rev[5];
+    id0x91_t        node[16];
+}__attribute__((packed)) id0x62_t;
+
+
+void imu_data_parse(id0x91_t *data, uint8_t *buf, uint32_t len);
 int imu_data_decode_init(void);
-int dump_rf_data(imu_data_t *imu);
+int dump_rf_data(id0x91_t *imu);
 
 #endif // CHHI22X_H
