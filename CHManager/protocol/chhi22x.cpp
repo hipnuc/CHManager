3bf031e05a90731cf8de514e43e3f8585f176e2f
yandld <yandld.hipnuc.com>
2020-05-15 23:04:17 +0800

    CHManager: protocol: update to support new items
    Update prococol to support 0x91 and 0x62 items
    
    Signed-off-by: yandld<yandld@hipnuc.com>

--- CHManager/protocol/chhi22x.cpp
+++ CHManager/protocol/chhi22x.cpp
@@ -6,36 +6,33 @@
 
 #include "chhi22x.h"
 #include "kptl.h"
-
 #include <string.h>
 
 static pkt_dec_t M_dec;
 static frame_packet_t rx_pkt;
-static imu_data_t imu_data;
+id0x91_t imu_data;
 
-/* default rx slave cnt */
-static uint8_t rf_slave_cnt = 12;
-
-typedef struct
-{
-    uint8_t flag;           /* 0:read  1:write */
-    uint8_t rsvd_addr;
-    uint8_t param_cnt;
-}ext_data_info_t;
-
-int dump_rf_data(imu_data_t *imu)
+int dump_rf_data(id0x91_t *imu)
 {
     *imu = imu_data;
 
     return 0;
 }
 
-/*  callback function of  when recv a data frame successfully */
-void imu_data_parse(imu_data_t *dat, uint8_t *buf, uint32_t len)
+/* convert binary stream to int16 array */
+static void stream2int16(int16_t *dat, uint8_t *buf)
 {
-    uint32_t offset;
+    dat[0] = (int16_t)(buf[0] | buf[1] << 8);
+    dat[1] = (int16_t)(buf[2] | buf[3] << 8);
+    dat[2] = (int16_t)(buf[4] | buf[5] << 8);
+}
+
+/*  callback function of  when recv a data frame successfully */
+void imu_data_parse(id0x91_t *dat, uint8_t *buf, uint32_t len)
+{
+    uint16_t offset, node_num;
+    int16_t tmp[3];
     uint8_t *p;
-    ext_data_info_t *ext = NULL;
     p = buf;
     offset = 0;
 
@@ -47,70 +44,58 @@ void imu_data_parse(imu_data_t *dat, uint8_t *buf, uint32_t len)
                 dat->id = p[1];
                 offset += 2;
                 break;
-            case kItemTest8F:
-                offset += 8 * 4 + 1;
-                break;
             case kItemAccRaw:
-            case kItemAccCalibrated:
-            case kItemAccFiltered:
-            case kItemAccLinear:
-                memcpy(dat->acc, p + offset + 1, sizeof(dat->acc));
+            case 0xA1:
+                stream2int16(tmp, &buf[offset + 1]);
+                dat->acc[0] = (float)tmp[0] / 1000;
+                dat->acc[1] = (float)tmp[1] / 1000;
+                dat->acc[2] = (float)tmp[2] / 1000;
                 offset += 7;
                 break;
             case kItemGyrRaw:
-            case kItemGyrCalibrated:
-            case kItemGyrFiltered:
-                memcpy(dat->gyr, p + offset + 1, sizeof(dat->gyr));
+            case 0xB1:
+                stream2int16(tmp, &buf[offset + 1]);
+                dat->gyr[0] = (float)tmp[0] / 10;
+                dat->gyr[1] = (float)tmp[1] / 10;
+                dat->gyr[2] = (float)tmp[2] / 10;
                 offset += 7;
                 break;
             case kItemMagRaw:
-            case kItemMagCalibrated:
-            case kItemMagFiltered:
-                memcpy(dat->mag, p + offset + 1, sizeof(dat->mag));
+            case 0xC1:
+                stream2int16(tmp, &buf[offset + 1]);
+                dat->mag[0] = (float)tmp[0] / 10;
+                dat->mag[1] = (float)tmp[1] / 10;
+                dat->mag[2] = (float)tmp[2] / 10;
                 offset += 7;
                 break;
             case kItemRotationEular:
-                dat->eul[0] = ((float)(int16_t)(p[offset+1] + (p[offset+2]<<8)))/100;
-                dat->eul[1] = ((float)(int16_t)(p[offset+3] + (p[offset+4]<<8)))/100;
-                dat->eul[2] = ((float)(int16_t)(p[offset+5] + (p[offset+6]<<8)))/10;
+                stream2int16(tmp, &buf[offset + 1]);
+                dat->eul[1] = (float)tmp[0] / 100;
+                dat->eul[0] = (float)tmp[1] / 100;
+                dat->eul[2] = (float)tmp[2] / 10;
                 offset += 7;
                 break;
-            case kItemRotationEular2:
-                //memcpy(eular, p + offset + 1, sizeof(eular));
-                offset += 13;
-                break;
             case kItemRotationQuat:
-                memcpy(dat->q, p + offset + 1, sizeof(dat->q));
+                memcpy((void*)dat->quat, p + offset + 1, sizeof(dat->quat));
                 offset += 17;
                 break;
             case kItemPressure:
                 offset += 5;
                 break;
-            case kItemTemperature:
-                memcpy(&dat->temperature, p + offset + 1, sizeof(dat->temperature));
-                offset += 5;
+            case kItemIMUSOL:
+                memcpy(dat, p + offset, sizeof(id0x91_t));
+                offset += 76;
                 break;
-            case kItemExtend:
-                ext = (ext_data_info_t*)(p + offset + 1);
-
-                /* get rf slave cnt */
-                rf_slave_cnt = ext->param_cnt;
-                offset += 4;
-                break;
-            case kItemRFQuat:
-                memcpy(&dat->rf_quat[0][0], p + offset + 1, sizeof(dat->rf_quat[0]) * rf_slave_cnt);
-                offset += 1 + sizeof(dat->rf_quat[0]) * rf_slave_cnt;
-                break;
-            case kItemRFEul:
-
-                memcpy(&dat->rf_eul[0][0], p + offset + 1, sizeof(dat->rf_eul[0]) * rf_slave_cnt);
-                offset += 1 + sizeof(dat->rf_eul[0]) * rf_slave_cnt;
+            case kItemRFSOL: /* TODO: RFSOL not parsed */
+                node_num = buf[offset+2];
+                offset += 8+76*node_num;
                 break;
             default:
-                offset++;
-                break;
+                //printf("data decode wrong\r\n");
+                return;
         }
     }
+
 }
 
 void kptl_cb(frame_packet_t *pkt)
@@ -126,4 +111,3 @@ int imu_data_decode_init(void)
 
     return 0;
 }
-
